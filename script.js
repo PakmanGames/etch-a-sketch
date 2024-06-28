eece755fec5796d0da77ad833dfd864f14f7440b
Andy Pak <147354924+PakmanGames@users.noreply.github.com>
2024-06-28 18:31:56 -0400

    Remove redundant code
    Removed unnecessary testing code and comments
    Added comments to explain script.js code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Grid and Elements for changing size
 const grid = document.querySelector(".grid");
 const input = document.querySelector(".input");
 const inputText = document.querySelector(".input-text");
@@ -13,16 +14,9 @@ const pastel = document.querySelector(".pastel");
 input.value = 16;
 setGrid(input.value);
 inputText.textContent = `x ${input.value}`;
-
 let tool = "greyscale";
 
-// const setSize = document.querySelector(".set-size");
-// setSize.addEventListener("click", () => {
-//     if (typeof(input.value) == "Number") {
-//         setGrid(input.value);
-//     }
-// });
-
+// Dynamically updates input text and grid size
 const updateInput = function() {
     if (!input.validity.valid) {
         if (input.value > 100) {
@@ -35,6 +29,7 @@ const updateInput = function() {
     setGrid(input.value);
 }
 
+// Event listeners for when user types value or uses input buttons
 input.addEventListener("keyup", updateInput);
 input.addEventListener("input", updateInput);
 
@@ -42,64 +37,67 @@ function getRandomColors() {
     return `${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}`;
 }
 
+// Sets size of grid and creates divs representing the tiles
 function setGrid(size) {
+    // Removes previous grid
     grid.replaceChildren();
+
     for (let i = 0; i < size*size; i++) {
         let tile = document.createElement("div");
         tile.style.height = `${600 / size - 2}px`
         tile.style.width = `${600 / size - 2}px`
         tile.className = "tile";
 
+        // Create event listeners for each tile
         tile.addEventListener("mouseover", () => {
+            // Opacity value and background-color value
             const opacity = tile.style.backgroundColor.slice(-4).substring(0,3);
             const bgColorCode = tile.style.backgroundColor;
 
-            const color = tile.style.backgroundColor.substring(5, tile.style.backgroundColor.length - 6);
-            // console.log(color);
-
+            // Different behaviour based on selected tool
             if (tool === "greyscale") {
                 if (opacity != "1" || !tile.classList.contains("greyscale")) {
+                    // If tile is fully coloured but is rgb
                     if (bgColorCode.includes("rgb(") && !tile.classList.contains("greyscale")) {
-                        tile.style.backgroundColor = `rgba(0, 0, 0, ${0.1})`;
-                        tile.classList.remove("colored");
-                    } else if (!tile.classList.contains("greyscale")) {
-                        tile.style.backgroundColor = `rgba(0, 0, 0, ${0.1})`;
-                    }
+                        tile.style.backgroundColor = `rgba(0, 0, 0, ${1})`;
+                    } 
                     tile.classList.remove("rgb");
                     tile.style.backgroundColor = `rgba(0, 0, 0, ${Number(opacity) + 0.1})`;
                     tile.classList.add("greyscale");
                 }
-            } else if (tool === "eraser") {
-                tile.style.backgroundColor = `rgba(255, 255, 255, ${0})`;
-                tile.className = "tile";
             } else if (tool === "rgb") {
                 if (opacity != "1" && !tile.classList.contains("rgb") || bgColorCode.includes("rgba(")) {
+                    // If tile is fully coloured but is grey
                     if (bgColorCode.includes("rgb(") && !tile.classList.contains("rgb")) {
-                        tile.style.backgroundColor = `rgba(${getRandomColors()}, ${0.1})`;
-                        tile.classList.remove("colored");
+                        tile.style.backgroundColor = `rgb(${getRandomColors()})`;
                     }
                     tile.classList.remove("greyscale");
                     tile.style.backgroundColor = `rgba(${getRandomColors()}, ${Number(opacity) + 0.1})`;
                     tile.classList.add("rgb");
                 }
+            } else if (tool === "eraser") {
+                // Change tile to white and reset class names
+                tile.style.backgroundColor = `rgba(255, 255, 255, ${0})`;
+                tile.className = "tile";
             }
+
+            // Once tile is fully coloured in
             if (opacity == "0.9") {
                 tile.classList.add("colored");
             }
         });
-
         grid.appendChild(tile);
     }
 }
 
+// Event listener for clear button with confirmation 
 clear.addEventListener("click", () => {
     if (confirm("Are you sure you want to clear the grid?")) {
         setGrid(input.value);
     }
 });
 
+// Event listeners for each of the tools
 eraser.addEventListener("click", () => tool = "eraser");
-
 greyscale.addEventListener("click", () => tool = "greyscale");
-
 rgb.addEventListener("click", () => tool = "rgb");
